de98d11e7e828e640c8e9c78c6ea506c307f4c54
Pablo Bañales <pablobanales@gmail.com>
2018-09-27 16:41:03 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var fetch = require('node-fetch');
+var app = express();
+
+app.get('/redirect', function (req, res) {
+    let url = req.query.url;
+    fetch(url)
+        .then(function (fetchRes) {
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ vimeoURL: fetchRes.url });
+        })
+        .catch(function (error) {
+            res.send(error);
+        });
+});
+
+app.listen(3000);
